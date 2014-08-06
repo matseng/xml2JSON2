790964ff1e3b773c43eeff51310eeee8d825b96f
Michael Tseng <mtseng@MTSENG-C02JH06ZDKQ2.local>
2014-08-06 13:56:28 -0700

    Added tests for basic xml to json parsing

--- tests/spec/appSpec.js
+++ tests/spec/appSpec.js
@@ -3,14 +3,28 @@
 (function () {
     'use strict';
 
-    describe('hello world', function () {
-        describe('maybe a bit more context here in the future', function () {
-            it('should return "hello world"', function () {
-            	expect(helloWorld()).to.equal('hello world 3');
-            });
-        });
-    });
+    describe('xml2json parser', function() {
+    	var x2js = new X2JS();
+    	
+    	describe('basic xml string to json examples', function() {
+    		var xmlString0 = "<root>Root 0</root>";
+    		var xmlString1 = "<root name-example='Root Name'><child>Child 1</child><child>Child 2</child></root>";
+    		var myJsonObj = x2js.xml_str2json(xmlString1);
+    		
+    		it('should have access to a root property', function() {
+    			expect(x2js.xml_str2json(xmlString0).root).to.equal('Root 0');
+    		});
 
+    		it('should have access to child properties', function() {
+    			expect(myJsonObj.root.child[1]).to.equal('Child 2');
+    			console.log(myJsonObj);
+    		});
 
+    		it('should access to a named attribute', function() {
+    			expect(myJsonObj.root['_name-example']).to.equal('Root Name');
+    		})
+    	})
+    	
+    })
 
 })();
